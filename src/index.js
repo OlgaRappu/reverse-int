e33ddcc8c1129177f97da32169182f026a8ab3cb
Ольга Дробышевская <95051051+OlgaRappu@users.noreply.github.com>
2022-02-16 19:41:24 +0000

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+n_str = String(Math.abs(n));
+let str = '';
+//str=n_str.split('');
+for(i=0; i<n_str.length; i++)
+str = str + n_str[n_str.length-i-1];
+//alert(str);
+str = Number(str); 
+return str; 
 }
